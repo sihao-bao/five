bf517d8f03c94e32a8b0cbf9c0178ee7242a8af9
周海天 <446056795@qq.com>
2022-09-05 20:32:23 +0800

    jia

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,19 @@
 <template>
   <div class="home">
-<<<<<<< HEAD
     <TopVue> </TopVue>
-=======
-    <Footer/>
->>>>>>> a24b4df08f02eb3385b4e61d417239eae76f5ad2
+    <Footer />
   </div>
 </template>
 <!-- javaScript: -->
 <script>
-<<<<<<< HEAD
 import TopVue from './Top.vue'
-export default {
-  name: 'Home',
-  components: {
-    TopVue,
-=======
 import Footer from '../views/Footer.vue'
 export default {
   name: 'Home',
   components: {
+    TopVue,
     Footer,
->>>>>>> a24b4df08f02eb3385b4e61d417239eae76f5ad2
   },
-  data() {
-    return {}
-  },
-  computed: {},
-  watch: {},
-  async created() {},
-  mounted() {},
-  methods: {},
 }
 </script>
 
